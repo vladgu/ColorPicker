7719e6abd901015dc698b2ad4c35d320a61cb025
Vladyslav Gulyaev <vlad040494@gmail.com>
2020-01-16 18:04:44 +0200

    almost there, without redux

--- src/containers/ColorPicker.jsx
+++ src/containers/ColorPicker.jsx
@@ -1,66 +1,67 @@
 import React, { useState, useEffect } from 'react'
 
 import colors from '../configs/colorList'
+import Input from '../components/ColorInput/Input'
+import PreviewBox from '../components/ColorPreviewBox/PreviewBox'
+import ArrowIcon from '../components/DropdownArrowIcon/ArrowIcon'
 import ListItem from '../components/ColorListItem/ListItem'
 import RangePicker from '../components/ColorRangePicker/RangePicker'
-import PreviewBox from '../components/ColorPreviewBox/PreviewBox'
 
 const ColorPicker = () => {
   const [value, onChange] = useState(colors[0].code)
+  const [rgbValue, onChangeRGB] = useState(colors[0].code)
   const [rgbDropdownOpened, onRGBDropdownOpen] = useState(false)
   const [dropdownOpened, onDropdownOpen] = useState(false)
 
   useEffect(() => {
-    // console.log(value, dropdownOpened, rgbDropdownOpened)
+    // console.log(value, rgbValue)
   })
 
   return (
     <div
-      className='app'
+      className='wrapper'
       onClick={() => {
         onRGBDropdownOpen(false)
         onDropdownOpen(false)
       }}>
       <div className='color-picker'>
-        <input type='text' className='color-input' value={value} onChange={e => onChange(e.target.value)} />
-        <div className='box'>
-          <PreviewBox
-            color={value}
-            rgbDropdownOpenedProp={rgbDropdownOpened}
-            onRGBDropdownOpenProp={onRGBDropdownOpen}
-            onDropdownOpenProp={onDropdownOpen}
-          />
-          {rgbDropdownOpened ? (
-            <div className='arrow-dropdown rgb'>
-              <div className='arrow-up rgb'></div>
-              <RangePicker currentColor={value} boxColorChange={onChange} />
-            </div>
-          ) : null}
-        </div>
-        <div className='box'>
-          <i
-            className={dropdownOpened ? 'arrow-down opened' : 'arrow-down'}
-            onClick={e => {
-              e.stopPropagation()
-              onDropdownOpen(!dropdownOpened)
-              onRGBDropdownOpen(false)
-            }}
-          />
-          {dropdownOpened ? (
-            <div className='arrow-dropdown'>
-              <div className='arrow-up'></div>
-              {colors.map((obj, index) => (
-                <ListItem
-                  key={String(index)}
-                  currentColor={value}
-                  colorName={obj.name}
-                  colorCode={obj.code}
-                  clickHandler={onChange}
-                />
-              ))}
-            </div>
-          ) : null}
-        </div>
+        <Input color={value !== rgbValue ? rgbValue : value} setColor={onChange} />
+
+        <PreviewBox
+          color={value !== rgbValue ? rgbValue : value}
+          rgbDropdownOpenedProp={rgbDropdownOpened}
+          onRGBDropdownOpenProp={onRGBDropdownOpen}
+          onDropdownOpenProp={onDropdownOpen}
+        />
+
+        {rgbDropdownOpened ? (
+          <div className='arrow-dropdown rgb'>
+            <div className='arrow-up rgb' />
+            <RangePicker currentColor={value} stateColorChange={onChange} preStateColorChange={onChangeRGB} />
+          </div>
+        ) : null}
+
+        <ArrowIcon
+          dropdownOpenedProp={dropdownOpened}
+          onRGBDropdownOpenProp={onRGBDropdownOpen}
+          onDropdownOpenProp={onDropdownOpen}
+        />
+
+        {dropdownOpened ? (
+          <div className='arrow-dropdown'>
+            <div className='arrow-up' />
+            {colors.map(({ name, code }, index) => (
+              <ListItem
+                key={String(index)}
+                currentColor={value}
+                colorName={name}
+                colorCode={code}
+                stateColorChange={onChange}
+                preStateColorChange={onChangeRGB}
+              />
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   )
